99cc78b299e690fc68e5884783c157c017aa4c3b
developedbyhardik <dev.hardik200@gmail.com>
2025-09-24 21:29:13 +0530

    TDD: sol 4

--- string_calculator.dart
+++ string_calculator.dart
@@ -9,5 +9,11 @@ int StringCalculator(String input) {
     input = input.split('\n')[1];
   }
 
+  final negativeNumbers = input.split(RegExp('[\n${delimiter}]')).where((e) => int.parse(e) < 0).toList();
+  
+  if (negativeNumbers.isNotEmpty) {
+    throw ArgumentError('negatives not allowed: ${negativeNumbers.join(', ')}');
+  }
+
   return input.split(RegExp('[\n${delimiter}]')).map(int.parse).reduce((acc, curr) => acc + curr);
 }
