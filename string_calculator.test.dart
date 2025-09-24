3cecdd3298cc08d83ae6b0750681460b4db6ff8d
developedbyhardik <dev.hardik200@gmail.com>
2025-09-24 21:19:47 +0530

    TDD: test 2

--- string_calculator.test.dart
+++ string_calculator.test.dart
@@ -11,4 +11,14 @@ void main() {
       expect(StringCalculator('1,2,3'), equals(6));
     });
   });
+
+  group('new line cases', () {
+    test('test 1: should return the sum of the numbers in the string', () {
+      expect(StringCalculator('1\n2,3'), equals(6));
+    });
+
+    test('test 2: should return the sum of the numbers in the string', () {
+      expect(StringCalculator('1\n2\n3,4\n5'), equals(15));
+    });
+  });
 }
