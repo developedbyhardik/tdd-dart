7039fdc874afc763a283f67b0956a251ccbf3b33
developedbyhardik <dev.hardik200@gmail.com>
2025-09-24 21:25:57 +0530

    TDD: sol 3

--- string_calculator.dart
+++ string_calculator.dart
@@ -3,5 +3,11 @@ int StringCalculator(String input) {
   if (input.isEmpty) {
     return 0;
   }
+
+  if (input.startsWith('//')) {
+    delimiter = input.split('\n')[0].substring(2);
+    input = input.split('\n')[1];
+  }
+
   return input.split(RegExp('[\n${delimiter}]')).map(int.parse).reduce((acc, curr) => acc + curr);
 }
